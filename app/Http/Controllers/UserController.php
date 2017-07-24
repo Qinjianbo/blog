4bfb5050b0db5b90363caf1f5e8899ed94f08a65
Qinjianbo <279250819@qq.com>
2017-07-24 14:37:48 +0000

    signin 增加验证

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,11 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
+use Validator;
+use App
 
 class UserController extends BaseController
 {
     public function signin(Request $request)
     {
+        $rules = [
+            'username' => 'required|string',
+            'password' => 'required|string',
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            $errors = $validator->errors();    
+
+            return $errors;
+        }
+        
         return 'signin';
     }
 
