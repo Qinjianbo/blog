359936b3b3416007c9a0adee88f88920b9d190a7
Qinjianbo <279250819@qq.com>
2017-07-24 14:48:26 +0000

    增加类注释

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,8 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
 use Validator;
-use App
+use App\User;
 
+/**
+ * UserController 
+ * 
+ * @uses BaseController
+ * PHP version 7
+ * 
+ * @package   App\Http\Controllers
+ * @author    Qinjianbo <279250819@qq.com> 
+ * @copyright 2016-2019 boboidea Co. All Rights Reserved.
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @version   GIT:<git_id>
+ * @link      https://www.boboidea.com
+ */
 class UserController extends BaseController
 {
     public function signin(Request $request)
