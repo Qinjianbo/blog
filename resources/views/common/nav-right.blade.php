fef2b298198515547bee49712b7f54c43f9f8bbd
Qinjianbo <279250819@qq.com>
2017-08-03 15:32:58 +0000

    添加登录按钮

---
+++ resources/views/common/nav-right.blade.php
@@ -0,0 +1,15 @@
+<div class="navbar-right">
+  <form method="GET" action="" class="navbar-form navbar-left hidden-sm hidden-md">
+    <div class="form-group">
+      <input class="form-control search-input" placeholder="搜索" name="q" type="text" value>
+    </div>
+  </form>
+
+  <ul class="nav navbar-nav">
+    <a class="btn btn-primary login-btn">
+      <i class="fa fa-user"></i>
+      登录
+    </a>
+  </ul>
+</div>
+
