56113deeeb6d40f8b617d044e416061b0ef69fec
Qinjianbo <279250819@qq.com>
2017-08-04 15:30:14 +0000

    增加登录后状态

--- resources/views/common/nav-right.blade.php
+++ resources/views/common/nav-right.blade.php
@@ -5,11 +5,38 @@
     </div>
   </form>
 
-  <ul class="nav navbar-nav">
+  <ul class="nav navbar-nav hidden">
     <a class="btn btn-primary login-btn">
       <i class="fa fa-user"></i>
       登录
     </a>
+  </ul>
+
+  <ul class="nav navbar-nav">
+    <li>
+      <a href="#" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="dLabel">
+        <img class="avatar-topnav" src="https://dn-phphub.qbox.me/uploads/avatars/15609_1492940220.png?imageView2/1/w/100/h/100"/>
+        秦建波
+        <span class="caret"></span>
+      </a>
+      <ul class="dropdown-menu" aria-labelledby="dLabel">
+        <li>
+          <a class="button" href="">
+              个人中心
+          </a>
+        </li>
+        <li>
+          <a class="button" href="">
+              编辑资料
+          </a>
+        </li>
+        <li>
+          <a class="button" id="login-out" href="">
+              退出
+          </a>
+        </li>
+      </ul>
+    </li>
   </ul>
 </div>
 
