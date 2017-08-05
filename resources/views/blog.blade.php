bc7b5e735579a42f724af09fd9259078b213908e
Qinjianbo <279250819@qq.com>
2017-08-05 08:46:14 +0000

    初步设置blog模板样式

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -6,7 +6,129 @@
 
 @section('content')
   <div class="container" style="margin-top:100px">
-    <h1>This is my Blog!</h1>
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            bootstrap panel 使用
+          </div>
+          <div class="panel-body">
+            很简单啦，使用panel 对应的类就可以啦！~~~
+          </div>
+          <div class="panel-footer">
+            这里是footer
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            bootstrap panel 使用
+          </div>
+          <div class="panel-body">
+            很简单啦，使用panel 对应的类就可以啦！~~~
+          </div>
+          <div class="panel-footer">
+            这里是footer
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            bootstrap panel 使用
+          </div>
+          <div class="panel-body">
+            很简单啦，使用panel 对应的类就可以啦！~~~
+          </div>
+          <div class="panel-footer">
+            这里是footer
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            bootstrap panel 使用
+          </div>
+          <div class="panel-body">
+            很简单啦，使用panel 对应的类就可以啦！~~~
+          </div>
+          <div class="panel-footer">
+            这里是footer
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            bootstrap panel 使用
+          </div>
+          <div class="panel-body">
+            很简单啦，使用panel 对应的类就可以啦！~~~
+          </div>
+          <div class="panel-footer">
+            这里是footer
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            bootstrap panel 使用
+          </div>
+          <div class="panel-body">
+            很简单啦，使用panel 对应的类就可以啦！~~~
+          </div>
+          <div class="panel-footer">
+            这里是footer
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            bootstrap panel 使用
+          </div>
+          <div class="panel-body">
+            很简单啦，使用panel 对应的类就可以啦！~~~
+          </div>
+          <div class="panel-footer">
+            这里是footer
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            bootstrap panel 使用
+          </div>
+          <div class="panel-body">
+            很简单啦，使用panel 对应的类就可以啦！~~~
+          </div>
+          <div class="panel-footer">
+            这里是footer
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            bootstrap panel 使用
+          </div>
+          <div class="panel-body">
+            很简单啦，使用panel 对应的类就可以啦！~~~
+          </div>
+          <div class="panel-footer">
+            这里是footer
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 @endsection
 
