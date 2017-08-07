bca5a56687bf43a8515d68afeae103831dbdb865
Qinjianbo <279250819@qq.com>
2017-08-07 15:02:29 +0000

    增加数据库监听，打印sql到日志

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        DB::listen(function ($query) {
+            $prepareSql = str_replace(['?', "\r\n", "\r", "\n"], ["'%s'", '', '', ''], $query->sql);
+            $prepareSql = preg_replace('/:[0-9a-z_]+/i', "'%s'", $prepareSql);
+            $sql = vsprintf($prepareSql, $query->bindings);
+            info(sprintf('sql:%s cost:%dms', $sql, $query->time));
+        });
     }
 
     /**
