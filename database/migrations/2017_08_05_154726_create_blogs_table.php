2b16f6443b1c09d6080186986ef8c9c5e2b1082e
Qinjianbo <279250819@qq.com>
2017-08-09 15:00:22 +0000

    博客字段设置默认值

--- database/migrations/2017_08_05_154726_create_blogs_table.php
+++ database/migrations/2017_08_05_154726_create_blogs_table.php
@@ -14,11 +14,11 @@ class CreateBlogsTable extends Migration
     public function up()
     {
         Schema::create('blogs', function (Blueprint $table) {
-            $table->increments('id')->comment('博客自增编号id');
-            $table->unsignedInteger('user_id')->comment('用户id');
+            $table->increments('id')->default(0)->comment('博客自增编号id');
+            $table->unsignedInteger('user_id')->default(0)->comment('用户id');
             $table->text('content')->comment('博客的内容');
-            $table->string('title')->comment('博客的标题');
-            $table->string('description')->comment('博客的简介');
+            $table->string('title')->default('')->comment('博客的标题');
+            $table->string('description')->default('')->comment('博客的简介');
             $table->unsignedTinyInteger('type')->default(1)->comment('1:原创 2:翻译');
             $table->unsignedInteger('reading')->default(0)->comment('该篇博客的阅读量');
             $table->unsignedInteger('comment_num')->default(0)->comment('该篇博客的评论量');
