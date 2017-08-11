496d6b689814dd2a8f0b671af0e0894795961ba4
Qinjianbo <279250819@qq.com>
2017-08-11 01:03:37 +0000

    增加User测试类

---
+++ tests/User/UserTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\User;
+
+use Tests\TestCase;
+
+/**
+ * UserTestCase  
+ * 
+ * @uses TestCase
+ * PHP version 7
+ * 
+ * @category  
+ * @package   
+ * @author    Qinjianbo <279250819@qq.com> 
+ * @copyright 2016-2019 boboidea Co. All Rights Reserved.
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @version   GIT:<git_id>
+ * @link      https://www.boboidea.com
+ */
+class UserTestCase extends TestCase
+{
+    /**
+     * testSignup 
+     * 
+     * 
+     * @access public
+     * 
+     * @return mixed
+     */
+    public function testSignup()
+    {
+        
+    }
+
+    /**
+     * testSignin 
+     * 
+     * 
+     * @access public
+     * 
+     * @return mixed
+     */
+     /**
+      * @depends testSignup
+      */
+    public function testSignin()
+    {
+        
+    }
+
+
+    /**
+     * testSignout 
+     * 
+     * 
+     * @access public
+     * 
+     * @return mixed
+     */
+     /**
+      * @depends testSignin
+      */
+    public function testSignout()
+    {
+        
+    }
+}
+>
