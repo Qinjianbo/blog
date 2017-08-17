5770127194a383e3c2c7260bd9a5d497b0655a14
Qinjianbo <279250819@qq.com>
2017-08-17 13:17:03 +0000

    增加注册js

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,7 +12,7 @@
 
     <script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="/js/signin.js"></script>
+    <script src="/js/common.js"></script>
     @yield('js')
   </body>
 </html>
