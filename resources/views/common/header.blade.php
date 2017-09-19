c011b0f4acdd8715ae1e53c77228be04cd16d04c
QinJianbo <279250819@qq.com>
2017-09-19 17:31:33 +0800

    公共模版增加关键字

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -2,6 +2,9 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="Jianbo Qin">
+  <meta name="keywords" content="@yield('keywords')">
+  <meta name="description" content="@yield('description')">
   <title>boboidea-@yield('title')</title>
   <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <link rel="stylesheet" href="/css/basic.css">
