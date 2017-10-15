761dd79c54243f995824d22b726536850346aa02
vagrant <vagrant@homestead>
2017-10-15 14:18:43 +0000

    去除无用分号

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,7 +9,7 @@
     </div>
 
     @include('common.signin')
-    @include('common.signup');
+    @include('common.signup')
 
     <script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
