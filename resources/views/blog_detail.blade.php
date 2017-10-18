464eba8340c4a0e900d0725cb56c6d7bd6ca5d05
QinJianbo <279250819@qq.com>
2017-10-18 20:59:17 +0800

    转载说明更改

--- resources/views/blog_detail.blade.php
+++ resources/views/blog_detail.blade.php
@@ -19,7 +19,7 @@
       </div>
       <div id="body-content" class="panel-body">
       </div>
-      <div>&nbsp;&nbsp;&nbsp;&nbsp;Copyright &copy; 2017 - 2017 boboidea.com All Rights Reserved 波波创意软件工作室 版权所有 【未经允许，谢绝转载】</div>
+      <div>&nbsp;&nbsp;&nbsp;&nbsp;Copyright &copy; 2017 - 2017 boboidea.com All Rights Reserved 波波创意软件工作室 版权所有 【转载请注明出处】</div>
       <div class="panel-footer">
           READ:{{$blog['reading']}}
       </div>
