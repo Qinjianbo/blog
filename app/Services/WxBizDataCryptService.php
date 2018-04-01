8706085021280e432cabfd699d891e50c4a4b7d2
QinJianbo <279250819@qq.com>
2018-04-01 17:50:45 +0800

    format

--- app/Services/WxBizDataCryptService.php
+++ app/Services/WxBizDataCryptService.php
@@ -1,7 +1,17 @@
 <?php
-namespace App\Services;
 
-/**
+/*
+ * This file is part of the openapi package.
+ *
+ * (c) 秦建波 <bobo@boboidea.com>,
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace app\Services;
+
+/*
  * 对微信小程序用户加密数据的解密示例代码.
  *
  * @copyright Copyright (c) 1998-2014 Tencent Inc.
@@ -13,9 +23,10 @@ class WxBizDataCryptService
 {
     private $appid;
     private $sessionKey;
-    
+
     /**
-     * 构造函数
+     * 构造函数.
+     *
      * @param $sessionKey string 用户在小程序登录后获取的会话密钥
      * @param $appid string 小程序的appid
      */
@@ -24,14 +35,15 @@ class WxBizDataCryptService
         $this->sessionKey = $sessionKey;
         $this->appid = $appid;
     }
-    
-    
+
     /**
      * 检验数据的真实性，并且获取解密后的明文.
+     *
      * @param $encryptedData string 加密的用户数据
      * @param $iv string 与用户数据一同返回的初始向量
      * @param $data string 解密后的原文
 
+     *
      * @return int 成功0，失败返回对应的错误码
      */
     public function decryptData($encryptedData, $iv, &$data)
@@ -39,26 +51,26 @@ class WxBizDataCryptService
         if (strlen($this->sessionKey) != 24) {
             return WxErrorCode::$IllegalAesKey;
         }
-        $aesKey=base64_decode($this->sessionKey);
-    
-    
+        $aesKey = base64_decode($this->sessionKey);
+
         if (strlen($iv) != 24) {
             return WxErrorCode::$IllegalIv;
         }
-        $aesIV=base64_decode($iv);
-    
-        $aesCipher=base64_decode($encryptedData);
-    
-        $result=openssl_decrypt($aesCipher, "AES-128-CBC", $aesKey, 1, $aesIV);
-    
-        $dataObj=json_decode($result);
-        if ($dataObj  == null) {
+        $aesIV = base64_decode($iv);
+
+        $aesCipher = base64_decode($encryptedData);
+
+        $result = openssl_decrypt($aesCipher, 'AES-128-CBC', $aesKey, 1, $aesIV);
+
+        $dataObj = json_decode($result);
+        if ($dataObj == null) {
             return WxErrorCode::$IllegalBuffer;
         }
         if ($dataObj->watermark->appid != $this->appid) {
             return WxErrorCode::$IllegalBuffer;
         }
         $data = $result;
+
         return WxErrorCode::$OK;
     }
 }
