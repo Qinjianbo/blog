d12ae00a8168276d1deb75ed61d3a957d56928a7
QinJianbo <279250819@qq.com>
2018-05-23 11:03:05 +0800

    版权时间变更

--- resources/views/blog/blog_detail.blade.php
+++ resources/views/blog/blog_detail.blade.php
@@ -23,7 +23,7 @@
       <div id="body-content" class="panel-body">
         {{$blog['content']}}
       </div>
-      <div>&nbsp;&nbsp;&nbsp;&nbsp;Copyright &copy; 2017 - 2017 boboidea.com All Rights Reserved 波波创意软件工作室 版权所有 【转载请注明出处】</div>
+      <div>&nbsp;&nbsp;&nbsp;&nbsp;Copyright &copy; 2017 - 2018 boboidea.com All Rights Reserved 波波创意软件工作室 版权所有 【转载请注明出处】</div>
     </div>
   </div>
 @endsection
