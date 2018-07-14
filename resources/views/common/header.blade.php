8b8bc3db5d81bfedef4d3870cbddcffa68bc4d52
QinJianbo <279250819@qq.com>
2018-07-14 23:53:34 +0800

    add baidu_uniton validation

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -11,4 +11,5 @@
   <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <link rel="stylesheet" href="/css/basic.css">
   @yield('css')
+  @yield('baidu_union')
 </head>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,10 @@
 
 @section('active_home', 'active')
 
+@section('baidu_union')
+<meta name="baidu_union_verify" content="07ed842e59fd6e58e8f9953920351d47">
+@endsection
+
 @section('content')
   <div class="container" style="margin-top:100px">
     <h1>This is my Content!</h1>
