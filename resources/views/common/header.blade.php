d3467acef38445d4b4fdcb42fa385e49166fa4c6
QinJianbo <279250819@qq.com>
2018-07-15 00:07:55 +0800

    remove baidu_union validation

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -1,5 +1,4 @@
 <head>
-  @yield('baidu_union')
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
