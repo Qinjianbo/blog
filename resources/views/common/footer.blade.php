7347014af6885128cbb6460a0219acd0297a7f6c
QinJianbo <279250819@qq.com>
2018-07-20 13:20:22 +0800

    增加备案信息 增加站长统计代码

---
+++ resources/views/common/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <div class="container">
+    <div class="row footer-top">
+      <div class="col-md-3 col-md-offset-3"><a href="http://www.miibeian.gov.cn">辽ICP备16004582号-1</a></div>
+      <div class="col-md-3">
+        <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1258070385'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s11.cnzz.com/z_stat.php%3Fid%3D1258070385%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
+      </div>
+    </div>
+  </div>
+</footer>
