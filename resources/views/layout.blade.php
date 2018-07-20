eb6c1d7b4fae81e726e27a0160e4b4f9b0183c25
QinJianbo <279250819@qq.com>
2018-07-20 13:22:09 +0800

    引入底部

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,6 +9,7 @@
     @include('common.signin')
     @include('common.signup')
 
+    @include('common.footer')
     <script src="https://cdn.staticfile.org/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="https://cdn.bootcss.com/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
