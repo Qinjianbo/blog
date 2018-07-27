9a481b944fb04b83cec79042da280dd5857390ab
QinJianbo <279250819@qq.com>
2018-07-27 16:59:37 +0800

    修复在about页面进行搜索url错误问题

--- resources/views/common/nav-right.blade.php
+++ resources/views/common/nav-right.blade.php
@@ -1,5 +1,5 @@
 <div class="navbar-right">
-  <form method="GET" action="" class="navbar-form navbar-left hidden-sm hidden-md">
+  <form method="GET" action="/" class="navbar-form navbar-left hidden-sm hidden-md">
     <div class="form-group">
       <input class="form-control search-input" placeholder="搜索" name="q" type="text" value="{{ $q ?? '' }}">
     </div>
