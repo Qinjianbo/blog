eb69410b9904c678903a5f36880993fe8cccc6ba
QinJianbo <279250819@qq.com>
2018-09-29 17:10:24 +0800

    登录校验中间件bug修复

--- app/Http/Middleware/CheckLogin.php
+++ app/Http/Middleware/CheckLogin.php
@@ -20,7 +20,7 @@ class CheckLogin
     {
         $key = sprintf('user_%s_%s', $request->get('user_id'), $request->get('device'));
         if (($user = collect(Cache::get($key)))->isEmpty() || !($user['user_id'] ?? 0)) {
-            return $this->result(collect(), '登陆超时或未登录', 102);
+            return response()->json(['code' => 102, 'msg' => '登录超时或未登录', 'data' => []], 401);
         }
         $request['user_id'] = $user['user_id'];
 
