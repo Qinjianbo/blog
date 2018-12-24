e3aee3943fa8500c0362ffbab1b39becffaf683a
jianbo.qin <jianbo.qin@dadaabc.com>
2018-12-24 19:45:13 +0800

    improve Article.vue

--- resources/assets/js/components/Article.vue
+++ resources/assets/js/components/Article.vue
@@ -11,14 +11,24 @@
 				<el-form-item label="正文">
 					<el-input type="textarea" :row="2" id="editor"></el-input>
 				</el-form-item>
+				<el-form-item label="tag">
+				    <el-input type="input"></el-input>
+				</el-form-item>
+				<el-form-item>
+				    <el-checkbox>原创请勾我</el-checkbox>
+				</el-form-item>
+				<el-button>保存</el-button>
+				<!--<router-link to="/articles"><el-button>取消</el-button></router-link>-->
 			</el-form>
 		</el-main>
 	</div>
 </template>
 <script type="text/javascript">
 	import {default as SimpleMDE} from 'simplemde/dist/simplemde.min';
+	import ElButton from "../../../../public/js/app";
 
 	export default {
+		components: {ElButton},
 		method() {
 			console.log('new article component mounted.');
 		},
